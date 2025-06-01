0959770de7392932f4fc2f7b1d061c4058475a9a
patrik.toka <tokapatrik321@gmail.com>
2025-06-01 22:12:21 +0200

    Add preloader to home page

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -7,6 +7,7 @@ import {
   httpBatchLink,
 } from "@trpc/react-query";
 
+import Spinner from "./features/shared/components/ui/Spinner";
 import { env } from "./lib/utils/env";
 import { routeTree } from "./routeTree.gen";
 
@@ -35,6 +36,11 @@ const createRouter = () => {
     context: {
       trpcQueryUtils,
     },
+    defaultPendingComponent: () => (
+      <div className="flex items-center justify-center">
+        <Spinner />
+      </div>
+    ),
     Wrap: ({ children }) => (
       <trpc.Provider client={trpcClinet} queryClient={queryClient}>
         <QueryClientProvider client={queryClient}>
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -6,25 +6,25 @@ import { trpc } from "@/router";
 
 export const Route = createFileRoute("/")({
   component: Index,
+  loader: async ({ context: { trpcQueryUtils } }) => {
+    await trpcQueryUtils.experiences.feed.prefetchInfinite({});
+  },
 });
 
 function Index() {
-  const experiencesQuery = trpc.experiences.feed.useInfiniteQuery(
-    {},
-    {
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    },
-  );
+  const [{ pages }, experiencesQuery] =
+    trpc.experiences.feed.useSuspenseInfiniteQuery(
+      {},
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
 
   return (
     <InfiniteScroll onLoadMore={experiencesQuery.fetchNextPage}>
       <ExperienceList
-        experiences={
-          experiencesQuery.data?.pages.flatMap((page) => page.experiences) ?? []
-        }
-        isLoading={
-          experiencesQuery.isLoading || experiencesQuery.isFetchingNextPage
-        }
+        experiences={pages.flatMap((page) => page.experiences)}
+        isLoading={experiencesQuery.isFetchingNextPage}
       />
     </InfiniteScroll>
   );
