dcf9e76f788e1ae7c8a4b2eaf17c42f23a069759
patrik.toka <tokapatrik321@gmail.com>
2025-06-11 23:03:41 +0200

    Add profile link to navbar

--- client/src/features/shared/components/Navbar.tsx
+++ client/src/features/shared/components/Navbar.tsx
@@ -35,6 +35,19 @@ function Navigation() {
         Search
       </Link>
 
+      {currentUser && (
+        <Link
+          to="/users/$userId"
+          params={{ userId: currentUser.id }}
+          variant="ghost"
+          className={navLinkClassName}
+          activeProps={{ className: activeNavLinkClassName }}
+        >
+          <User className="h-6 w-6" />
+          Profile
+        </Link>
+      )}
+
       {currentUser ? (
         <Link
           to="/settings"
