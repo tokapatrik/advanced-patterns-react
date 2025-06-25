0fe9739f810010dec62e894da64072cbd6dd73c0
patrik.toka <tokapatrik321@gmail.com>
2025-06-25 13:25:30 +0200

    Implementing unfollowing users

--- client/src/features/users/components/UserFollowButton.tsx
+++ client/src/features/users/components/UserFollowButton.tsx
@@ -1,6 +1,7 @@
 import { User } from "@advanced-react/server/database/schema";
 import { useParams } from "@tanstack/react-router";
 
+import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 import { Button } from "@/features/shared/components/ui/Button";
 import { useToast } from "@/features/shared/hooks/useToast";
 import { trpc } from "@/router";
@@ -16,6 +17,7 @@ export function UserFollowButton({
 }: UserFollowButtonProps) {
   const { toast } = useToast();
   const utils = trpc.useUtils();
+  const { currentUser } = useCurrentUser();
 
   const { userId: pathUserId } = useParams({ strict: false });
   const { experienceId: pathExperienceId } = useParams({ strict: false });
@@ -168,6 +170,158 @@ export function UserFollowButton({
     },
   });
 
+  const unfollowMutation = trpc.users.unfollow.useMutation({
+    onMutate: async ({ id }) => {
+      function updateUser<
+        T extends { isFollowing: boolean; followersCount: number },
+      >(oldData: T) {
+        return {
+          ...oldData,
+          isFollowing: false,
+          followersCount: Math.max(oldData.followersCount - 1, 0),
+        };
+      }
+
+      await Promise.all([
+        utils.users.byId.cancel({ id }),
+        ...(pathUserId
+          ? [
+              utils.users.followers.cancel({ id: pathUserId }),
+              utils.users.following.cancel({ id: pathUserId }),
+            ]
+          : []),
+        ...(pathExperienceId
+          ? [
+              utils.users.experienceAttendees.cancel({
+                experienceId: pathExperienceId,
+              }),
+            ]
+          : []),
+      ]);
+
+      const previousData = {
+        byId: utils.users.byId.getData({ id }),
+        ...(pathUserId
+          ? {
+              followers: utils.users.followers.getInfiniteData({
+                id: pathUserId,
+              }),
+              following: utils.users.following.getInfiniteData({
+                id: pathUserId,
+              }),
+            }
+          : {}),
+        ...(pathExperienceId
+          ? {
+              experienceAttendees:
+                utils.users.experienceAttendees.getInfiniteData({
+                  experienceId: pathExperienceId,
+                }),
+            }
+          : {}),
+      };
+
+      utils.users.byId.setData({ id }, (oldData) => {
+        if (!oldData) {
+          return;
+        }
+
+        return updateUser(oldData);
+      });
+
+      if (pathUserId) {
+        utils.users.followers.setInfiniteData({ id: pathUserId }, (oldData) => {
+          if (!oldData) {
+            return;
+          }
+
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              items: page.items.map((user) =>
+                user.id === id ? updateUser(user) : user,
+              ),
+            })),
+          };
+        });
+
+        utils.users.following.setInfiniteData({ id: pathUserId }, (oldData) => {
+          if (!oldData) {
+            return;
+          }
+
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              items: page.items.map((user) =>
+                user.id === id ? updateUser(user) : user,
+              ),
+            })),
+          };
+        });
+      }
+
+      if (pathExperienceId) {
+        utils.users.experienceAttendees.setInfiniteData(
+          {
+            experienceId: pathExperienceId,
+          },
+          (oldData) => {
+            if (!oldData) {
+              return;
+            }
+
+            return {
+              ...oldData,
+              pages: oldData.pages.map((page) => ({
+                ...page,
+                attendees: page.attendees.map((user) =>
+                  user.id === id ? updateUser(user) : user,
+                ),
+              })),
+            };
+          },
+        );
+      }
+
+      return { previousData };
+    },
+    onError: (error, { id }, context) => {
+      utils.users.byId.setData({ id }, context?.previousData.byId);
+
+      if (pathUserId) {
+        utils.users.followers.setInfiniteData(
+          { id: pathUserId },
+          context?.previousData.followers,
+        );
+
+        utils.users.following.setInfiniteData(
+          { id: pathUserId },
+          context?.previousData.following,
+        );
+      }
+
+      if (pathExperienceId) {
+        utils.users.experienceAttendees.setInfiniteData(
+          { experienceId: pathExperienceId },
+          context?.previousData.experienceAttendees,
+        );
+      }
+
+      toast({
+        title: "Failed to unfollow user",
+        description: error.message,
+        variant: "destructive",
+      });
+    },
+  });
+
+  if (!currentUser || currentUser.id === targetUserId) {
+    return null;
+  }
+
   return (
     <Button
       variant={isFollowing ? "outline" : "default"}
@@ -175,12 +329,12 @@ export function UserFollowButton({
         e.preventDefault();
 
         if (isFollowing) {
-          // TODO: Implement unfollow
+          unfollowMutation.mutate({ id: targetUserId });
         } else {
           followMutation.mutate({ id: targetUserId });
         }
       }}
-      disabled={followMutation.isPending}
+      disabled={followMutation.isPending || unfollowMutation.isPending}
     >
       {isFollowing ? "Unfollow" : "Follow"}
     </Button>
